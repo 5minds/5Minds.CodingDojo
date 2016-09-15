99c03e90274fc67f42cfbec98df8f35440183c6a
maddin-5minds <martin.poepel@5minds.de>
2016-09-15 23:52:49 +0200

    Add csharp fizz buzz solution

---
+++ katas/FizzBuzz/solutions/maddin/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.RunExtended(1, 15);
+            Console.ReadLine();
+        }
+    }
+}
