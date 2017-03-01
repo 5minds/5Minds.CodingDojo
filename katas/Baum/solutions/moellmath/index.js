d0ab26250fe474112219e76195a944f150d1f3f0
Heiko Mathes <heiko.mathes@5minds.de>
2017-03-01 18:09:14 +0100

    :sparkles: Finish him

--- katas/Baum/solutions/moellmath/index.js
+++ katas/Baum/solutions/moellmath/index.js
@@ -5,17 +5,60 @@ class Tree {
     this.right = null;
   }
 
-  addLeft(value) {
-    this.left = new Tree(value);
-    return this.left;
+  addValues(values) {
+    for (let value of values) {
+      this.addValue(value);
+    }
   }
 
-  addRight(value) {
-    this.right = new Tree(value);
-    return this.right;
+  addValue(value) {
+    let currentTree = this;
+    let lastTree = null;
+    let lastDirectionisLeft = false;
+
+    while (currentTree) {
+      lastTree = currentTree;
+      if (value < currentTree.value) {
+        currentTree = currentTree.left;
+        lastDirectionisLeft = true;
+      } else if (value > currentTree.value) {
+        currentTree = currentTree.right;
+        lastDirectionisLeft = false;
+      } else {
+        console.log(`${value} already exists`);
+        return;
+      }
+    }
+    
+    if (lastDirectionisLeft) {
+      lastTree.left = new Tree(value);
+    } else {
+      lastTree.right = new Tree(value);
+    }
+  }
+
+  findValue(value) {
+    let currentTree = this;
+    let lastDirectionisLeft = false;
+    const directions = [];
+
+    while (currentTree) {
+      if (value < currentTree.value) {
+        currentTree = currentTree.left;
+        directions.push('left');
+      } else if (value > currentTree.value) {
+        currentTree = currentTree.right;
+        directions.push('right');
+      } else {
+        return directions;
+      }
+    }
+
+    console.log(`${value} not found`);
   }
 }
 
-const tree = new Tree(30)
-tree.addLeft(15).addLeft(7);
-console.log(tree);
+const tree = new Tree(30);
+tree.addValues([15, 20, 7, 60, 11, 40, 79]);
+const path = tree.findValue(11);
+console.log(path);
