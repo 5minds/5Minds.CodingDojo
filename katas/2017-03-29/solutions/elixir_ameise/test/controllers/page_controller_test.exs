470bffa9c49e16f5cb4254ad398f340d9747d30f
René Föhring <rene.foehring@5minds.de>
2017-03-31 11:32:29 +0200

    :ant: Add coding dojo solution "Ameise.W0rld"

---
+++ katas/2017-03-29/solutions/elixir_ameise/test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Ameise.PageControllerTest do
+  use Ameise.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
---
+++ katas/2017-03-29/solutions/elixir_ameise/web/views/layout_view.ex
@@ -0,0 +1,3 @@
+defmodule Ameise.LayoutView do
+  use Ameise.Web, :view
+end
