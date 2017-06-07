f71c3ca15efaf420b4f82fd5ed939565e71b7344
Daniel Heise <danielgheise@gmail.com>
2017-06-07 20:10:13 +0200

    Update

--- katas/2017-06-07_BrainFuck/solutions/Daniel/Brainfuck/BrainfuckInterpreter.cs
+++ katas/2017-06-07_BrainFuck/solutions/Daniel/Brainfuck/BrainfuckInterpreter.cs
@@ -8,12 +8,14 @@ namespace Brainfuck
     {
         private readonly Dictionary<int, int> memory = new Dictionary<int, int> { { 0, 0 } };
 
+        private string inputSequence;
         private int memoryPosition = 0;
         private int inputSequencePosition = 0;
+        private int bracketCount = 0;
 
-        public void Process(string inputSequence)
+        public void Process(string input)
         {
-            int bracketCount = 0;
+            inputSequence = input;
 
             while (inputSequencePosition < inputSequence.Length)
             {
@@ -22,68 +24,119 @@ namespace Brainfuck
                 switch (instruction)
                 {
                     case '>':
-                        memoryPosition++;
-                        if (!memory.ContainsKey(memoryPosition))
-                            memory.Add(memoryPosition, 0);
+                        MoveMemoryPositionForward();
                         break;
 
                     case '<':
-                        memoryPosition--;
+                        MoveMemoryPositionBackward();
                         break;
 
                     case '+':
-                        memory[memoryPosition] = memory[memoryPosition] + 1;
+                        IncrementCurrentMemoryCell();
                         break;
 
                     case '-':
-                        memory[memoryPosition] = memory[memoryPosition] - 1;
+                        DecrementCurrentMemoryCell();
                         break;
 
                     case '.':
-                        Console.Write(Encoding.ASCII.GetString(new[] { (byte)memory[memoryPosition] }));
+                        WriteCurrentMemoryValueToConsole();
                         break;
 
                     case ',':
-                        memory[memoryPosition] = Console.Read();
+                        ReadCurrentMemoryValueFromConsole();
                         break;
 
                     case '[':
-                        if (memory[memoryPosition] == 0)
-                        {
-                            bracketCount = 0;
-                            do
-                            {
-                                int c = inputSequence[inputSequencePosition++];
-                                if (c == '[')
-                                    bracketCount++;
-                                else if (c == ']' && bracketCount == 0)
-                                    break;
-                                else if (c == ']')
-                                    bracketCount--;
-                            }
-                            while (true);
-                        }
+                        SeekToClosingBracketIfNeeded();
                         break;
 
                     case ']':
-                        if (memory[memoryPosition] != 0)
-                        {
-                            inputSequencePosition--;
-                            bracketCount = 0;
-                            do
-                            {
-                                int c = inputSequence[--inputSequencePosition];
-                                if (c == ']')
-                                    bracketCount++;
-                                else if (c == '[' && bracketCount == 0)
-                                    break;
-                                else if (c == '[')
-                                    bracketCount--;
-                            }
-                            while (true);
-                        }
+                        SeekBeforeOpeningBracketIfNeeded();
                         break;
                 }
+            }
+        }
+
+        private void MoveMemoryPositionForward()
+        {
+            memoryPosition++;
+            ExpandMemoryIfNeeded();
+        }
+
+        private void ExpandMemoryIfNeeded()
+        {
+            if (!memory.ContainsKey(memoryPosition))
+            {
+                memory.Add(memoryPosition, 0);
+            }
+        }
+
+        private void MoveMemoryPositionBackward()
+        {
+            memoryPosition--;
+        }
+
+        private void IncrementCurrentMemoryCell()
+        {
+            memory[memoryPosition] = memory[memoryPosition] + 1;
+        }
+
+        private void DecrementCurrentMemoryCell()
+        {
+            memory[memoryPosition] = memory[memoryPosition] - 1;
+        }
+
+        private void WriteCurrentMemoryValueToConsole()
+        {
+            Console.Write((char)memory[memoryPosition]);
+        }
+
+        private void ReadCurrentMemoryValueFromConsole()
+        {
+            memory[memoryPosition] = Console.Read();
+        }
+
+        private void SeekToClosingBracketIfNeeded()
+        {
+            if (memory[memoryPosition] == 0)
+            {
+                bracketCount = 0;
+
+                do
+                {
+                    int c = inputSequence[inputSequencePosition++];
+
+                    if (c == '[')
+                        bracketCount++;
+                    else if (c == ']' && bracketCount == 0)
+                        break;
+                    else if (c == ']')
+                        bracketCount--;
+                }
+                while (true);
+            }
+        }
+
+        private void SeekBeforeOpeningBracketIfNeeded()
+        {
+            if (memory[memoryPosition] != 0)
+            {
+                inputSequencePosition--;
+                bracketCount = 0;
+
+                do
+                {
+                    int c = inputSequence[--inputSequencePosition];
+
+                    if (c == ']')
+                        bracketCount++;
+                    else if (c == '[' && bracketCount == 0)
+                        break;
+                    else if (c == '[')
+                        bracketCount--;
+                }
+                while (true);
             }
         }
     }
