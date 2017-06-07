dcee4c20b0c614ef7c71733cf39b0c8d88084397
Daniel Heise <danielgheise@gmail.com>
2017-06-07 19:49:29 +0200

    Add Brainfuck_Kata

---
+++ katas/2017-06-07_BrainFuck/solutions/Daniel/Brainfuck/BrainfuckInterpreter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brainfuck
+{
+    public sealed class BrainfuckInterpreter
+    {
+        private readonly Dictionary<int, int> memory = new Dictionary<int, int> { { 0, 0 } };
+
+        private int memoryPosition = 0;
+        private int inputSequencePosition = 0;
+
+        public void Process(string inputSequence)
+        {
+            int bracketCount = 0;
+
+            while (inputSequencePosition < inputSequence.Length)
+            {
+                char instruction = inputSequence[inputSequencePosition++];
+
+                switch (instruction)
+                {
+                    case '>':
+                        memoryPosition++;
+                        if (!memory.ContainsKey(memoryPosition))
+                            memory.Add(memoryPosition, 0);
+                        break;
+
+                    case '<':
+                        memoryPosition--;
+                        break;
+
+                    case '+':
+                        memory[memoryPosition] = memory[memoryPosition] + 1;
+                        break;
+
+                    case '-':
+                        memory[memoryPosition] = memory[memoryPosition] - 1;
+                        break;
+
+                    case '.':
+                        Console.Write(Encoding.ASCII.GetString(new[] { (byte)memory[memoryPosition] }));
+                        break;
+
+                    case ',':
+                        memory[memoryPosition] = Console.Read();
+                        break;
+
+                    case '[':
+                        if (memory[memoryPosition] == 0)
+                        {
+                            bracketCount = 0;
+                            do
+                            {
+                                int c = inputSequence[inputSequencePosition++];
+                                if (c == '[')
+                                    bracketCount++;
+                                else if (c == ']' && bracketCount == 0)
+                                    break;
+                                else if (c == ']')
+                                    bracketCount--;
+                            }
+                            while (true);
+                        }
+                        break;
+
+                    case ']':
+                        if (memory[memoryPosition] != 0)
+                        {
+                            inputSequencePosition--;
+                            bracketCount = 0;
+                            do
+                            {
+                                int c = inputSequence[--inputSequencePosition];
+                                if (c == ']')
+                                    bracketCount++;
+                                else if (c == '[' && bracketCount == 0)
+                                    break;
+                                else if (c == '[')
+                                    bracketCount--;
+                            }
+                            while (true);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
