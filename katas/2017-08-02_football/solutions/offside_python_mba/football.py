dca61dd04a2d8bb6c682b00e485f7883fee56191
Marc Biegota <marc.biegota@5minds.de>
2017-08-02 20:11:10 +0200

    :sparkles: pre-final

--- katas/2017-08-02_football/solutions/offside_python_mba/football.py
+++ katas/2017-08-02_football/solutions/offside_python_mba/football.py
@@ -1,6 +1,9 @@
 
+offSidePossible = True
+defenders = 0
+
 def main():
-    lines = readFile('examples\situation_4.txt')
+    lines = readFile('solutions/offside_python_mba/examples/situation_5.txt')
 
     hasPlayer = scanFieldForPlayer(lines)
     if hasPlayer:        
@@ -17,29 +20,46 @@ def readFile(filename):
     lines = open(filename).readlines()
     return lines
 
-def scanLineForOffside(lines, scanline):
+def scanLineForOffside(field, scanline):
     defenseCount = 0
     offenseCount = 0
+    lastDefensePosition = 0
+    global offSidePossible
+    global defenders
     for position in range(0, 45):
-        if lines[position][scanline] == 'x':
-            defenseCount = defenseCount + 1
-        elif lines[position][scanline] == '+':
-            offenseCount = offenseCount + 1
+        #print field[position]
+        #print position, scanline
+        if field[position][scanline] == 'x':
+            if lastDefensePosition == 0:
+                lastDefensePosition = position
+                if field[position][scanline-1] != 'x':
+                    defenseCount = defenseCount + 1
+            elif lastDefensePosition + 2 < position:
+                defenseCount = defenseCount + 1
+                lastDefensePosition = lastDefensePosition + 1
+        elif field[position][scanline] == '+':
+            offenseCount = 1
+
+    if defenseCount > 0:
+        defenders = defenders + 1
+
+    if defenseCount > 1 or defenders > 1:
+        offSidePossible = False
 
     if (offenseCount >= defenseCount) and (offenseCount > 0):
         return True
 
     return False
 
-def scanFieldForOffside(lines):
+def scanFieldForOffside(field):
     offside = False
     scanline = 60
 
-    while (not offside) and scanline > 30:
-        offside = scanLineForOffside(lines, scanline)
+    while offSidePossible and (not offside) and scanline > 30:
+        offside = scanLineForOffside(field, scanline)
         scanline = scanline - 1
 
-    return offside
+    return offside and offSidePossible
 
 def scanFieldForPlayer(lines):
     hasPlayer = False
