57309636cca57613b0bc378d089f443966fc48a7
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-27 20:20:58 +0200

    :package: add brutal c# solution

--- katas/2017-09-27_wacken/solutions/heiko_dotnet_core/Program.cs
+++ katas/2017-09-27_wacken/solutions/heiko_dotnet_core/Program.cs
@@ -1,12 +1,88 @@
-﻿using System;
-
-namespace wacken
+﻿namespace wacken
 {
+    using System;
+    using System.Collections.Generic;
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Path> situation1 = new List<Path>(){
+                new Path(){point1 = "point1", point2 = "point2"},
+                new Path(){point1 = "point1", point2 = "point3"},
+                new Path(){point1 = "point1", point2 = "point4"},
+                new Path(){point1 = "point2", point2 = "point3"},
+                new Path(){point1 = "point2", point2 = "point5"},
+                new Path(){point1 = "point3", point2 = "point4"},
+                new Path(){point1 = "point4", point2 = "point5"},
+                new Path(){point1 = "point4", point2 = "point7"},
+                new Path(){point1 = "point4", point2 = "point8"},
+                new Path(){point1 = "point5", point2 = "point6"},
+                new Path(){point1 = "point5", point2 = "point7"},
+                new Path(){point1 = "point5", point2 = "point8"},
+                new Path(){point1 = "point6", point2 = "point7"},
+                new Path(){point1 = "point7", point2 = "point8"}
+            };
+
+            List<Path> situation2 = new List<Path>(){
+                new Path(){point1 = "point1", point2 = "point2"},
+                new Path(){point1 = "point1", point2 = "point3"},
+                new Path(){point1 = "point1", point2 = "point4"},
+                new Path(){point1 = "point1", point2 = "point5"},
+
+                new Path(){point1 = "point2", point2 = "point3"},
+                new Path(){point1 = "point2", point2 = "point4"},
+                new Path(){point1 = "point2", point2 = "point5"},
+
+                new Path(){point1 = "point3", point2 = "point4"},
+                new Path(){point1 = "point3", point2 = "point7"},
+
+                new Path(){point1 = "point4", point2 = "point8"},
+
+                new Path(){point1 = "point5", point2 = "point6"},
+                new Path(){point1 = "point5", point2 = "point7"},
+
+                new Path(){point1 = "point6", point2 = "point7"},
+                new Path(){point1 = "point7", point2 = "point8"},
+            };
+
+            List<string> points = new List<string>(){ "point1", "point2", "point3", "point4", "point5", "point6", "point7", "point8"};
+
+            testSituation(situation2, points);
+
+        }
+
+        static void testSituation(List<Path> paths, List<string> points) {
+            foreach (string point in points) {
+                findPossiblePaths(point, paths, point, new List<string>(){point});
+            }
+        }
+
+        static void findPossiblePaths(string startPoint, List<Path> situation, string currentPoint, List<string> route) {
+            List<Path> currentSituation = new List<Path>(situation);
+
+            if (currentSituation.Count == 0 && startPoint == currentPoint) {
+                Console.WriteLine("path found");
+                foreach (string routePoint in route) {
+                    Console.Write(routePoint + " -> ");
+                }
+            }
+
+            foreach (Path path in currentSituation) {
+                if (path.point1 != currentPoint && path.point2 != currentPoint) {
+                    continue;
+                }
+
+                string targetPoint = path.point1;
+                if (path.point1 == currentPoint) {
+                    targetPoint = path.point2;
+                }
+                
+                List<Path> remainingPossiblePaths = new List<Path>(currentSituation);
+                List<string> currentRoute = new List<string>(route);
+                currentRoute.Add(targetPoint);
+                remainingPossiblePaths.Remove(path);
+                findPossiblePaths(startPoint, remainingPossiblePaths, targetPoint, currentRoute);
+            }
         }
     }
 }
