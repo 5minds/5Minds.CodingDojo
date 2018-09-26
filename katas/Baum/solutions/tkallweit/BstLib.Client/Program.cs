1522741d4c1cf60e483213851a91732699706015
Thorsten Kallweit <thorsten.kallweit@bms-software.de>
2018-09-26 20:07:21 +0200

    Binäre Suchbaum-Bibliothek, Unit Tests und Client-Console hinzugefügt

---
+++ katas/Baum/solutions/tkallweit/BstLib.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BstLib.Client
+{
+    using System;
+    
+
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            int[] values = new int[] { 30, 15, 60, 7, 20, 14, 79, 11 };
+            Tree root = new Tree(values);
+            
+            var result = root.Sort();
+            foreach (var element in result)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine($"{root.Find(55)}");
+        }
+    }
+}
