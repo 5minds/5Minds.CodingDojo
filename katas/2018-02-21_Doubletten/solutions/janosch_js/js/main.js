1137f153e41e306a4484840b7b985ead510f3b6c
Janosch Zbick <janosch.zbick@gmail.com>
2018-10-12 12:19:20 +0200

    added select for checking a candidate

--- katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
+++ katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
@@ -1,8 +1,22 @@
 document.getElementById("filepicker").addEventListener("change", function(event) {
   let doublicate = new Duplicate(event.target.files);
-  let sel_mod = document.getElementById("sel_mod");
+  const sel_mod = document.getElementById("sel_mod");
   const mod = sel_mod.options[sel_mod.selectedIndex].value;
+  
+  const sel_doublicate = document.getElementById("sel_doublicate");
+  sel_doublicate.options.length = 0;
+  sel_doublicate.innerHTML = "";
+  
   console.log(doublicate.getCandidates(mod));
+  const candidates = doublicate.getCandidates(mod);
+  candidates.forEach((candidate, index) => {
+    sel_doublicate.options[sel_doublicate.options.length] = new Option(candidate.name, index);
+  });
+  
+  sel_doublicate.addEventListener("change", function () {
+    console.log(candidates[sel_doublicate.options[sel_doublicate.selectedIndex].value]);
+  }, false);
   
   document.getElementById("filepicker").value = '';
 }, false);
+
