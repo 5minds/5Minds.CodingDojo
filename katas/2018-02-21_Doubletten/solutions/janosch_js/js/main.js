902e82d91065fbb12f49e94414d11ca3af92b649
Janosch Zbick <janosch.zbick@gmail.com>
2018-10-12 11:43:34 +0200

    added a select element to chose mod method

--- katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
+++ katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
@@ -1,5 +1,8 @@
 document.getElementById("filepicker").addEventListener("change", function(event) {
   let doublicate = new Duplicate(event.target.files);
-  console.log(doublicate.getCandidates("name"));
+  let sel_mod = document.getElementById("sel_mod");
+  const mod = sel_mod.options[sel_mod.selectedIndex].value;
+  console.log(doublicate.getCandidates(mod));
   
+  document.getElementById("filepicker").value = '';
 }, false);
