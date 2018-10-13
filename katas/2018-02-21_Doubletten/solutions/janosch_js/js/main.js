861b4e41110c7d404ffc638d9373af21969fb95f
Janosch Zbick <janosch.zbick@gmail.com>
2018-10-13 11:31:18 +0200

    show dublicates in a list

--- katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
+++ katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
@@ -1,4 +1,5 @@
 document.getElementById("filepicker").addEventListener("change", function(event) {
+  clearList();
   let doublicate = new Duplicate(event.target.files);
   const sel_mod = document.getElementById("sel_mod");
   const mod = sel_mod.options[sel_mod.selectedIndex].value;
@@ -13,10 +14,26 @@ document.getElementById("filepicker").addEventListener("change", function(event)
   });
   
   sel_doublicate.addEventListener("change", function () {
+    clearList();
     checkDouplicate();
     async function checkDouplicate() {
       await doublicate.checkCandidate(candidates[sel_doublicate.options[sel_doublicate.selectedIndex].value]).then(res => {
-        console.log(res)
+        console.log(res);
+        const ul_coublicate = document.getElementById("ul_doublicate");
+        for (let key in res) {
+          if (res.hasOwnProperty(key)) {
+            let val = res[key];
+            console.log("key", key);
+            console.log("val", val);
+            let li = document.createElement("li");
+            let liText = "Filename: "+val[0].name+" located here:";
+            val.forEach(item => {
+              liText += ' "'+item.webkitRelativePath+'" ';
+            });
+            li.appendChild(document.createTextNode(liText));
+            ul_coublicate.appendChild(li);
+          }
+        }
       });
     }
     //console.log(doublicate.checkCandidate(candidates[sel_doublicate.options[sel_doublicate.selectedIndex].value]));
@@ -24,4 +41,9 @@ document.getElementById("filepicker").addEventListener("change", function(event)
   
   document.getElementById("filepicker").value = '';
 }, false);
+
+function clearList(){
+  const ul_coublicate = document.getElementById("ul_doublicate");
+  ul_coublicate.innerHTML = "";
+}
 
