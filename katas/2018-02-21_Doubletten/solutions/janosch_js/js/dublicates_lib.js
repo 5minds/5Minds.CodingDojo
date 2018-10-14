91dd6d5db2387b14b644bcbe52c0ce2e23ccbbd4
Janosch Zbick <janosch.zbick@gmail.com>
2018-10-14 12:49:26 +0200

    added comments

--- katas/2018-02-21_Doubletten/solutions/janosch_js/js/dublicates_lib.js
+++ katas/2018-02-21_Doubletten/solutions/janosch_js/js/dublicates_lib.js
@@ -7,7 +7,7 @@ class Duplicate {
   //filtering the candidates from the filelist. By default candidates will be filtered by name and size, if the mod parameter is provided with 'name', candidates will be filtered only by name
   getCandidates(mod = 'name+size'){
     
-    let candidates = [];
+    const candidates = [];
     //create an array with only filenames
     const fileNameArray = Array.from(this.fileList).map(file => file.name);
     
@@ -32,6 +32,7 @@ class Duplicate {
       });
     }
     
+    //check dandidates by name and size
     if (mod === "name+size"){
       let newCandidates = [];
       candidates.forEach(can => {
@@ -60,7 +61,9 @@ class Duplicate {
     
   }
   
+  //check candidates by md5
   async checkCandidate(candidate){
+    //create the md5 value for all candidates as promises
     function createMd5FromFiles() {
       const fileReaders = [];
       for (let i = 0; i < candidate.files.length; ++i) {
@@ -80,11 +83,13 @@ class Duplicate {
           fileReader.readAsBinaryString(file);
         }))
       }
+      //return when all promises are resolved
       return Promise.all(fileReaders);
     }
     
     const md5s = await createMd5FromFiles();
     
+    //create the return value as a json with the md5 as key and all files with that md5 in an array as value
     function filterDuplicates(candidates) {
       const dublicates = {};
       for (let i=0; i < candidates.length; i++){
@@ -96,6 +101,7 @@ class Duplicate {
       }
       return dublicates;
     }
+    //return the generated json
     return await filterDuplicates(md5s);
   }
 }
--- katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
+++ katas/2018-02-21_Doubletten/solutions/janosch_js/js/main.js
@@ -2,36 +2,35 @@ const candidates = [];
 const dublicate = {};
 
 document.getElementById("filepicker").addEventListener("change", (event) => {
+  //empty fields from previous searches
   clear();
-  window.dublicate = new Duplicate(event.target.files);
-  const sel_mod = document.getElementById("sel_mod");
-  const mod = sel_mod.options[sel_mod.selectedIndex].value;
-  
   const sel_dublicate = document.getElementById("sel_dublicate");
   sel_dublicate.options.length = 0;
   sel_dublicate.innerHTML = "";
   
+  //initiate library
+  window.dublicate = new Duplicate(event.target.files);
+  //read the selected search mod
+  const mod = document.getElementById("sel_mod").options[document.getElementById("sel_mod").selectedIndex].value;
+  
+  //initiate the search for candidates
   window.candidates = window.dublicate.getCandidates(mod);
+  //add options to the select for all identified candidates
   window.candidates.forEach((candidate, index) => {
     sel_dublicate.options[sel_dublicate.options.length] = new Option(candidate.name, index);
   });
   
-  const btn_check = document.getElementById("btn_check");
-  btn_check.disabled = false;
+  //enable the check button and reset the filepicker
+  document.getElementById("btn_check").disabled = false;
   document.getElementById("filepicker").value = '';
 }, false);
 
-function clear(){
-  const ul_coublicate = document.getElementById("ul_dublicate");
-  ul_coublicate.innerHTML = "";
-}
-
 function checkCandidates() {
-  const sel_dublicate = document.getElementById("sel_dublicate");
-  const candidates = window.candidates;
-  
+  //clear list of previous searches
   clear();
+  const sel_dublicate = document.getElementById("sel_dublicate");
   
+  //check if candidates are identified
   if (!sel_dublicate.options[sel_dublicate.selectedIndex]){
     alert("No doublicates identified at this folder, please chose another folder.");
   } else {
@@ -39,9 +38,9 @@ function checkCandidates() {
   }
   
   async function checkDuplicate() {
-    console.log(candidates[sel_dublicate.options[sel_dublicate.selectedIndex].value]);
-    await window.dublicate.checkCandidate(candidates[sel_dublicate.options[sel_dublicate.selectedIndex].value]).then(res => {
-      const ul_dublicate = document.getElementById("ul_dublicate");
+    //call the function that checks the md5 of the candidates
+    await window.dublicate.checkCandidate(window.candidates[sel_dublicate.options[sel_dublicate.selectedIndex].value]).then(res => {
+      //display all identified dublicates in a list with the path for each duplicate
       for (let key in res) {
         if (res.hasOwnProperty(key)) {
           let val = res[key];
@@ -51,10 +50,15 @@ function checkCandidates() {
             liText += ' "'+item.webkitRelativePath+'" ';
           });
           li.appendChild(document.createTextNode(liText));
-          ul_dublicate.appendChild(li);
+          document.getElementById("ul_dublicate").appendChild(li);
         }
       }
     });
   }
+}
+
+function clear(){
+  const ul_coublicate = document.getElementById("ul_dublicate");
+  ul_coublicate.innerHTML = "";
 }
 
