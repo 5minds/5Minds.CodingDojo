9c01f3860f9447f58da8a7db506dd2cdef145afe
Reichelt <hartmut.reichelt@bms-software.de>
2019-03-19 10:19:28 +0100

    Implementierung Kata Doubletten

---
+++ katas/2018-02-21_Doubletten/solutions/Reichelt_20190319/FileDuplicateFinder/CompareModeEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace FileDuplicateFinder
+{
+    public enum CompareModeEnum
+    {
+        // Compare files by file name
+        FileSize,
+
+        // Compare files by file size and file name
+        FileSize_FileName
+    }
+}
