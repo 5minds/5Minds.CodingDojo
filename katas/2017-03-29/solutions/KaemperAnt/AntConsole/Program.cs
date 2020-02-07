1b915c62f25c01354aa0fb732121e01a66462d87
Andreas Kämper <andreas@kaemper.tech>
2020-02-07 17:39:05 +0100

    Erste funktionierende Implementation des Langton's Ant Katas

---
+++ katas/2017-03-29/solutions/KaemperAnt/AntConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AntConsole
+{
+    class Program
+    {
+        private const int DEFAULT_BOARD_SIZE = 11;
+        private const int DEFAULT_START_X = 5;
+        private const int DEFAULT_START_Y = 5;
+        private const int DEFAULT_START_DIR = 0;
+        private const int DEFAULT_STEPS = 50;
+
+        static void Main(string[] args)
+        {
+            int boardSize = DEFAULT_BOARD_SIZE;
+            int startX = DEFAULT_START_X;
+            int startY = DEFAULT_START_Y;
+            int startDir = DEFAULT_START_DIR;
+            int steps = DEFAULT_STEPS;
+
+            if (args.Length == 5)
+            {
+                boardSize = int.Parse(args[0]);
+                startX = int.Parse(args[1]);
+                startY = int.Parse(args[2]);
+                startDir = int.Parse(args[3]);
+                steps = int.Parse(args[4]);
+            }
+
+            var sim = new AntSimulator();
+            var filename = sim.Simulate(boardSize, startX, startY, startDir, steps);
+
+            Console.WriteLine(string.Format("Result of simulation saved in file '{0}'", filename));
+        }
+    }
+}
---
+++ katas/2017-03-29/solutions/KaemperAnt/AntGui/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Threading;
+
+namespace AntGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int FIELD_SIZE = 32;
+
+        private List<Rectangle> fieldRectangles;
+        private Queue<string> steps;
+
+        private int boardSize;
+
+        private DispatcherTimer stepTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            stepTimer = new DispatcherTimer();
+            stepTimer.Tick += StepTimer_Tick;
+        }
+
+        private void DrawBoard(string stepLine)
+        {
+            var stepBoard = new List<string>(stepLine.Split(','));
+
+            for (int i = 0; i < stepBoard.Count; i++)
+            {
+                var fieldColor = stepBoard[i];
+                var antDir = "";
+
+                if (stepBoard[i].Length == 2)
+                {
+                    antDir = stepBoard[i][0].ToString();
+                    fieldColor = stepBoard[i][1].ToString();
+                }
+
+                if (!string.IsNullOrEmpty(antDir))
+                {
+                    Canvas.SetTop(imgAnt, Canvas.GetTop(fieldRectangles[i]));
+                    Canvas.SetLeft(imgAnt, Canvas.GetLeft(fieldRectangles[i]));
+
+                    switch (antDir)
+                    {
+                        case "n":
+                            imgAnt.RenderTransform = new RotateTransform(0);
+                            break;
+                        case "o":
+                            imgAnt.RenderTransform = new RotateTransform(90);
+                            break;
+                        case "s":
+                            imgAnt.RenderTransform = new RotateTransform(180);
+                            break;
+                        case "w":
+                            imgAnt.RenderTransform = new RotateTransform(270);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    imgAnt.Visibility = Visibility.Visible;
+                }
+
+                if (fieldColor == "w")
+                {
+                    fieldRectangles[i].Fill = Brushes.White;
+                }
+                else if (fieldColor == "s")
+                {
+                    fieldRectangles[i].Fill = Brushes.Black;
+                }
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtFileName.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            steps = new Queue<string>(File.ReadAllLines(txtFileName.Text));
+
+            stepTimer.Interval = TimeSpan.FromSeconds(double.Parse(txtStepDelay.Text));
+
+            boardSize = (int)Math.Sqrt(steps.Peek().Split(',').Count());
+
+            // If Canvas is already populated, clear the old rectangles
+            if (fieldRectangles != null && fieldRectangles.Any())
+            {
+                foreach (var rect in fieldRectangles)
+                {
+                    canvasBoard.Children.Remove(rect);
+                }
+
+                fieldRectangles.Clear();
+            }
+
+            // Populate the Canvas with rectangles to represent the board fields
+            fieldRectangles = new List<Rectangle>();
+
+            for (int y = 0; y < boardSize; y++)
+            {
+                for (int x = 0; x < boardSize; x++)
+                {
+                    var rect = new Rectangle
+                    {
+                        Width = FIELD_SIZE,
+                        Height = FIELD_SIZE,
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 1,
+                        Fill = Brushes.White
+                    };
+
+                    canvasBoard.Children.Add(rect);
+                    fieldRectangles.Add(rect);
+                    Canvas.SetTop(rect, FIELD_SIZE * y);
+                    Canvas.SetLeft(rect, FIELD_SIZE * x);
+                    Panel.SetZIndex(rect, 0);
+                }
+            }
+
+            stepTimer.Start();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void StepTimer_Tick(object sender, EventArgs e)
+        {
+            if (steps.Any())
+            {
+                DrawBoard(steps.Dequeue());
+            }
+            else
+            {
+                stepTimer.Stop();
+            }
+        }
+    }
+}
