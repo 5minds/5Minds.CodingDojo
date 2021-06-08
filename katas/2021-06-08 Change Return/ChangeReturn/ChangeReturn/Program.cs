f80136ce9ece8be50c749e679512ca31868267dc
Marius <Mariusjahn@web.de>
2021-06-08 11:56:52 +0200

    Projektstart für C#;
    Programmcode in Java beigelegt.

---
+++ katas/2021-06-08 Change Return/ChangeReturn/ChangeReturn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChangeReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
