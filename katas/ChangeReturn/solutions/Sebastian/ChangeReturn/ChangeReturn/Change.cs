1ed7e8864f5ca9c7a97889f47c0939d81e3d4da1
smash <smash@trioptimum.com>
2021-09-29 00:15:32 +0200

    added solution for ChangeReturn

---
+++ katas/ChangeReturn/solutions/Sebastian/ChangeReturn/ChangeReturn/Change.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChangeReturn
+{
+    public class Change
+    {
+        /// <summary>
+        /// This List holds the change that will be retured, it contains bills or coins and their respective count.
+        /// For expampe: 100 Euro Bill times 2, 50 Cent coin times 3, and so on.
+        /// </summary>
+        public List<(Money Money, int Count)> ChangeMoney { get; private set; }
+
+        public Change()
+        {
+            ChangeMoney = new List<(Money, int)>();
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                var total = ChangeMoney.Sum(x => x.Money.NumericalValue * x.Count);
+
+                return total;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (ChangeMoney.Count == 0)
+                return "no change";
+
+            var builder = new StringBuilder();
+
+            foreach (var item in ChangeMoney)
+            {
+                builder.AppendFormat("{0}x {1}, ", item.Count, item.Money);
+            }
+
+            var result = builder.ToString();
+            result = result.TrimEnd(' ', ',');
+
+            return result.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Change compare)
+            {
+                if (ChangeMoney.Count != compare.ChangeMoney.Count)
+                    return false;
+
+                for (int i = 0; i < ChangeMoney.Count; i++)
+                {
+                    if (ChangeMoney[i].Money != compare.ChangeMoney[i].Money ||
+                        ChangeMoney[i].Count != compare.ChangeMoney[i].Count)
+                        return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return System.HashCode.Combine(ChangeMoney);
+        }
+
+    }
+}
---
+++ katas/ChangeReturn/solutions/Sebastian/ChangeReturn/ChangeReturnTests/ChangeCalculatorTests.cs
@@ -0,0 +1,115 @@
+﻿using ChangeReturn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static ChangeReturn.Money;
+
+namespace ChangeReturn.Tests
+{
+    [TestClass()]
+    public class ChangeCalculatorTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestForExcpetionWhenChangeIsImpossible()
+        {
+            // that would be on 100th of a cent, which is impossible for us to return
+            var result = ChangeCalculator.ReturnChange((decimal)0.0001);
+            Assert.IsNull(result);
+        }
+
+        [DataTestMethod()]
+        [DataRow(-0.1f)]
+        [DataRow(-1f)]
+        [DataRow(-10f)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestForExcpetionWhenChangeIsNotPositve(float changeAmount)
+        {
+            var result = ChangeCalculator.ReturnChange((decimal)changeAmount);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestForExcpetionWhenExistingAmountsArrayIsInvalid()
+        {
+            // -1 is not allowed
+            decimal[] amountsArray = { 100m, 10m, -1m };
+
+            var result = new Change();
+            decimal remainingAmount = 0m;
+            ChangeCalculator.AddFittingMoneyAmountsToResult(result, ref remainingAmount, amountsArray, MoneyType.EuroBill);
+        }
+
+        [TestMethod()]
+        public void ReturnChangeTest00()
+        {
+            var expected = new Change();
+            var result = ChangeCalculator.ReturnChange(0);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ReturnChangeTest01()
+        {
+            var expected = new Change();
+            expected.ChangeMoney.Add((new Money(100, MoneyType.EuroBill), 1));
+            expected.ChangeMoney.Add((new Money(20, MoneyType.EuroBill), 1));
+
+            var result = ChangeCalculator.ReturnChange(120);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ReturnChangeTest02()
+        {
+            var expected = new Change();
+            expected.ChangeMoney.Add((new Money(100, MoneyType.EuroBill), 2));
+            expected.ChangeMoney.Add((new Money(20, MoneyType.EuroBill), 1));
+            expected.ChangeMoney.Add((new Money(5, MoneyType.EuroBill), 1));
+
+            var result = ChangeCalculator.ReturnChange(225);
+            Assert.AreEqual(expected, result);
+        }
+
+        //[TestMethod()]
+        //public void ReturnChangeTest03()
+        //{
+        //    var expected = new Change();
+        //    expected.ChangeMoney.Add((new Money(100, MoneyType.EuroBill), 1));
+        //    expected.ChangeMoney.Add((new Money(20, MoneyType.EuroBill), 1));
+        //    expected.ChangeMoney.Add((new Money(2, MoneyType.EuroCoin), 1));
+        //    expected.ChangeMoney.Add((new Money(1, MoneyType.EuroCoin), 1));
+        //    expected.ChangeMoney.Add((new Money(0.2m, MoneyType.CentCoin), 2));
+        //    expected.ChangeMoney.Add((new Money(0.05m, MoneyType.CentCoin), 1));
+
+        //    var result = ChangeCalculator.ReturnChange((decimal)123.45);
+        //    Assert.AreEqual(expected, result);
+        //}
+
+        [TestMethod()]
+        public void ReturnChangeTest03()
+        {
+            var expected = new Change();
+            expected.ChangeMoney.Add((new Money(100, MoneyType.EuroBill), 1));
+            expected.ChangeMoney.Add((new Money(20, MoneyType.EuroBill), 1));
+            expected.ChangeMoney.Add((new Money(0.5m, MoneyType.CentCoin), 6));
+            expected.ChangeMoney.Add((new Money(0.2m, MoneyType.CentCoin), 2));
+            expected.ChangeMoney.Add((new Money(0.05m, MoneyType.CentCoin), 1));
+
+            var result = ChangeCalculator.ReturnChange((decimal)123.45);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ReturnChangeTest04()
+        {
+            var expected = new Change();
+            expected.ChangeMoney.Add((new Money(0.02m, MoneyType.CentCoin), 1));
+            expected.ChangeMoney.Add((new Money(0.01m, MoneyType.CentCoin), 1));
+
+            var result = ChangeCalculator.ReturnChange((decimal)0.03);
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
