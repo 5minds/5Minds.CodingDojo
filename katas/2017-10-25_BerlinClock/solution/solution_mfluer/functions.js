ca639dd778d1339353e74e95f0ba1194557855ed
Markus Flür <fluer.markus@googlemail.com>
2021-10-23 13:16:13 +0200

    feat: added small visual project to berlin-clock

---
+++ katas/2017-10-25_BerlinClock/solution/solution_mfluer/functions.js
@@ -0,0 +1,31 @@
+function parsetime(now) {
+    const hourFives = Math.floor(now.getHours() / 5);
+    const hourRest = now.getHours() % 5
+    const minuteFives = Math.floor(now.getMinutes() / 5);
+    const minuteRest = now.getMinutes() % 5;
+    const second = now.getSeconds() % 2;
+
+    return {
+        hourFives,
+        hourRest,
+        minuteFives,
+        minuteRest,
+        second
+    }
+}
+
+function renderSecond(timeObjectSecond) {
+    return timeObjectSecond === 0 ? "⊙" : "⬤";
+}
+
+function renderFourBlock(timeObjectHours) {
+    return "█".repeat(parseInt(timeObjectHours, 10)).padEnd(4, "▁")
+}
+
+function renderFiftyFive(timeObjectMinutes) {
+    return "■".repeat(parseInt(timeObjectMinutes, 10)).padEnd(55, "▢")
+}
+
+module.exports = {
+    parsetime, renderSecond, renderFourBlock, renderFiftyFive
+}
--- katas/2017-10-25_BerlinClock/solution/solution_mfluer/solution_node.js
+++ katas/2017-10-25_BerlinClock/solution/solution_mfluer/solution_node.js
@@ -1,34 +1,6 @@
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin);
-
-function parsetime(now) {
-    const hourFives = Math.floor(now.getHours() / 5);
-    const hourRest = now.getHours()%5
-    const minuteFives = Math.floor(now.getMinutes()/5);
-    const minuteRest = now.getMinutes() % 5;
-    const second = now.getSeconds() % 2;
-
-    return {
-        hourFives,
-        hourRest,
-        minuteFives,
-        minuteRest,
-        second
-    }
-}
-
-function renderSecond(timeObjectSecond) {
-    return timeObjectSecond === 0 ? "⊙" : "⬤";
-}
-
-function renderFourBlock(timeObjectHours) {
-    return "█".repeat(parseInt(timeObjectHours,10)).padEnd(4,"▁")
-}
-
-function renderFiftyFive(timeObjectMinutes) {
-    return "■".repeat(parseInt(timeObjectMinutes, 10)).padEnd(55,"▢")
-}
-
+const {parsetime, renderSecond, renderFourBlock, renderFiftyFive} = require('./functions')
 function renderClockLine({
     hourFives,
     hourRest,
