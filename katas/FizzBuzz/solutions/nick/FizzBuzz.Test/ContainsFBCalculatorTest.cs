0c2273c2919da9c6c689c6798529bddacc48b700
Ehsan Faramarzi <netpro@email.com>
2021-10-31 13:16:51 +0100

    FizzBuzz.Test added; tests successfull

---
+++ katas/FizzBuzz/solutions/nick/FizzBuzz.Test/ContainsFBCalculatorTest.cs
@@ -0,0 +1,50 @@
+using FizzBuzz.Server.Providers;
+using FizzBuzz.Shared.Domain.Model.Enums;
+using FizzBuzz.Shared.Domain.Providers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+    [TestClass]
+    public class ContainsFBCalculatorTest
+    {
+        [DataTestMethod]
+        [DataRow(37)]
+        [DataRow(13)]
+        [DataRow(33)]
+        public void TestFizz(int number)
+        {
+            IFBCalculator fBCalculator = new ContainsFBCalculator();
+            Assert.AreEqual(fBCalculator.Get(number), EFizzBuzz.Fizz);
+        }
+
+        [DataTestMethod]
+        [DataRow(59)]
+        [DataRow(25)]
+        [DataRow(55)]
+        public void TestBuzz(int number)
+        {
+            IFBCalculator fBCalculator = new ContainsFBCalculator();
+            Assert.AreEqual(fBCalculator.Get(number), EFizzBuzz.Buzz);
+        }
+
+        [DataTestMethod]
+        [DataRow(53)]
+        [DataRow(35)]
+        public void TestFizzBuzz(int number)
+        {
+            IFBCalculator fBCalculator = new ContainsFBCalculator();
+            Assert.AreEqual(fBCalculator.Get(number), EFizzBuzz.FizzBuzz);
+        }
+
+        [DataTestMethod]
+        [DataRow(28)]
+        [DataRow(1)]
+        public void TestNone(int number)
+        {
+            IFBCalculator fBCalculator = new ContainsFBCalculator();
+            Assert.AreEqual(fBCalculator.Get(number), EFizzBuzz.None);
+        }
+    }
+}
