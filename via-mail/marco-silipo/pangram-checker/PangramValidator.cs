bd42b6bfb6457b916ea1c14df28ca92710b5781f
X39 <X39@users.noreply.github.com>
2021-11-06 21:19:27 +0100

    pangram-checker
    addition of PangramValidator class
    addition of test for a potential IsPangram method with Quick-Brown-Fox pangram
    making the QuickBrownFoxIsPangram test pass
    addition of abc IsPangram test
    making Abc-Is-Pangram test pass
    addition of common library project
    addition of test for string extension method RandomizeCasing
    making RandomizeCasingTest pass
    addition of Ranomize-Casing-With-Umlauts test
    addition of Abc-Random-Casing-Is-Pangram test
    making Abc-Random-Casing-Is-Pangram test pass
    addition of test String-Of-Only-A-Is-No-Pangram
    making String-Of-Only-A-Is-No-Pangram test pass
    addition of Abc-Is-Perfect-Pangram test
    making Abc-Is-Perfect_pangram pass
    addition of Quick-Brown-Fox-Is-Imperfect-Pangram test
    making Quick-Brown-Fox-Is-Imperfect-Pangram test pass
    addition of Abc-Random-Casing-Is-Perfect-Pangram test
    making abc-random-casing-is-perfect-pangram test pass
    addition of Shuffle-Characters test for string extension method ShuffleChars
    making Shuffle-Characters-Not-Equal-Test test pass
    addition of Shuffle-Characters-Sequence-Not-Contained test
    addition of Shuffle-Characters-Sequence-Reversed-Not-Contained test
    making of Shuffle-Characters-Sequence-Reversed-Not-Contained test pass
    addition of Abc-Shuffled-Is-Perfect-Pangram test
    making Abc-Shuffled-Is-Perfect-Pangram test pass
    addition of String-Of-Only-A-Is-Invalid-Pangram test
    making String-Of-Only-A-Is-Invalid-Pangram test pass
    implementating pangram checker into console application
    refactored duplicate code into single, privately scoped method

---
+++ via-mail/marco-silipo/pangram-checker/PangramValidator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PangramChecker
+{
+    public class PangramValidator
+    {
+        // ReSharper disable once StringLiteralTypo
+        const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+
+        private Dictionary<char, int> GetPangramCounts(string potentialPangram)
+        {
+            var counts = Alphabet.ToDictionary((c) => c, (_) => 0);
+            foreach (var c in potentialPangram.Where(char.IsLetter).Select(char.ToLower))
+            {
+                counts[c]++;
+            }
+
+            return counts;
+        }
+        public bool IsPangram(string potentialPangram)
+        {
+            var counts = GetPangramCounts(potentialPangram);
+
+            return counts.All((kvp) => kvp.Value > 0);
+        }
+        
+        public EPangramType GetPangramType(string potentialPangram)
+        {
+            var counts = GetPangramCounts(potentialPangram);
+
+            return counts.Any((kvp) => kvp.Value == 0)
+                ? EPangramType.Invalid
+                : counts.Any((kvp) => kvp.Value > 1)
+                ? EPangramType.Imperfect
+                : EPangramType.Perfect;
+        }
+    }
+}
