3f53e306ad07fdde91f74cc28a74bba3a19b1aad
DESKTOP-VBJD9JN\alexb <Daniel Schoettgen>
2021-11-07 14:41:35 +0100

    Add file handler for reading possible palindromes adde

--- katas/Palindrom/solutions/dschoettgen/csharp/csharp/Program.cs
+++ katas/Palindrom/solutions/dschoettgen/csharp/csharp/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace csharp
 {
     class Program
     {
+        const string defaultPalindromeSourceFile = @"Resources\Palindrome.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+
+                string sourceFile = string.Empty;
+
+               
+
+
+                  
+                    sourceFile = args.FirstOrDefault() == ":F" ? args[1] : defaultPalindromeSourceFile   ;
+                
+
+                if (File.Exists(sourceFile))
+                {
+                    foreach (string line in File.ReadLines(sourceFile))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("Die Datei {0} konnte nicht gefunden werden.", sourceFile));
+                }
+
+                
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Bei der Ausführung ist ein Fehler aufgetreten: " + e.Message);
+            }
         }
     }
 }
