e213abfa1feca74877d299cf4fd292b3bd761ca0
DESKTOP-VBJD9JN\alexb <Daniel Schoettgen>
2021-11-07 16:25:52 +0100

    add class palindrome, add method isPalidrome, remove launch seetings argument for debug, change console messages to english, extend default txt file to check condition

---
+++ katas/Palindrom/solutions/dschoettgen/csharp/csharp/Classes/Palindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp.Classes
+{
+    /// <summary>
+    /// Class Palindrome: Check if a given string a Palindrome
+    /// </summary>
+    public class Palindrome
+    {
+        private string givenValue;
+        private string invertedValue;
+
+        private readonly char[] punctuationAndSpace  = new char[]{
+            ' ',
+            ',',
+            '.',
+            ':',
+            '!',
+            '?',
+            ';',
+            '-',
+            '(',
+            ')',
+            '/'
+        };
+
+        /// <summary>
+        /// Check if a given string a palindrome
+        /// </summary>
+        /// <param name="value">a string to compare</param>
+        /// <returns>bool</returns>
+        public bool IsPalindrome(string value)
+        {
+            if(!string.IsNullOrEmpty(value))
+            {
+                givenValue = string.Join("", value.Split(punctuationAndSpace)).ToLower();
+                char[] charArrayGivenValue = givenValue.ToCharArray();
+                Array.Reverse(charArrayGivenValue);
+                invertedValue = new string(charArrayGivenValue);
+
+                if (givenValue == invertedValue)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
--- katas/Palindrom/solutions/dschoettgen/csharp/csharp/Program.cs
+++ katas/Palindrom/solutions/dschoettgen/csharp/csharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using csharp.Classes;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -8,38 +9,33 @@ namespace csharp
     {
         const string defaultPalindromeSourceFile = @"Resources\Palindrome.txt";
 
+        /// <summary>
+        /// This little programm reads strings line by line from a given file and check if the line a palindrome and show the result as console output
+        /// </summary>
+        /// <param name="args">string ':F path/to/file'</param>
         static void Main(string[] args)
         {
             try
             {
-
-                string sourceFile = string.Empty;
-
-               
-
-
-                  
-                    sourceFile = args.FirstOrDefault() == ":F" ? args[1] : defaultPalindromeSourceFile   ;
-                
+                string sourceFile = sourceFile = args.FirstOrDefault() == ":F" ? args[1] : defaultPalindromeSourceFile;
 
                 if (File.Exists(sourceFile))
                 {
+                    Palindrome palindrome = new Palindrome();
+
                     foreach (string line in File.ReadLines(sourceFile))
                     {
-                        Console.WriteLine(line);
+                        Console.WriteLine(string.Format("{0} : {1}",line, palindrome.IsPalindrome(line)));
                     }
                 }
                 else
                 {
-                    Console.WriteLine(string.Format("Die Datei {0} konnte nicht gefunden werden.", sourceFile));
+                    Console.WriteLine(string.Format("The file {0} could not be found.", sourceFile));
                 }
-
-                
-                
             }
             catch (Exception e)
             {
-                Console.WriteLine("Bei der Ausführung ist ein Fehler aufgetreten: " + e.Message);
+                Console.WriteLine("An error occurred during execution: " + e.Message);
             }
         }
     }
