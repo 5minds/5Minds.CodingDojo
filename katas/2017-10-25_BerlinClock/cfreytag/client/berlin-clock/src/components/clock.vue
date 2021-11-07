9390296efe6217a66c9d8da4de7ec9a81c9e8fb5
Christopher <Christopher.Freytag@gmx.de>
2021-11-07 02:06:07 +0100

    rough sketch

---
+++ katas/2017-10-25_BerlinClock/cfreytag/client/berlin-clock/src/components/clock.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="clock">
+    <div class="seconds"></div>
+    <ul class="Hours-Base-Five">
+      <li class="active" v-for="hour in hoursBaseFive" :key="hour"></li>
+      <li v-for="n in hoursBaseFiveInactives" :key="n"><li>
+    </ul>
+    <ul class="Hours-Base-Five-Remainder">
+      <li></li>
+    </ul>
+    <ul class="Minutes-Base-Five">
+      <li></li>
+    </ul>
+    <ul class="Minutes-Base-Five-Remainder">
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BerlinClock',
+  props: {
+    msg: String,
+    berlinClockTime: BerlinClockFormat,
+  },
+  data: {
+
+  },
+  computed: {
+    hoursFiveInactives: 4 - berlinClockTime.hours,
+    hoursRemainderInactives: 4 - berlinClockTime.hoursRemainder,
+    minutesFiveInactives: 11 - berlinClockTime.minutes,
+    minutesRemainderInactives: 4 - berlinClockTime.minutesRemainder,
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
