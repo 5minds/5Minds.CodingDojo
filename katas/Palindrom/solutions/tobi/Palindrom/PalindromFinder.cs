12f190518d7b6c995e35297bf394154262a1a27f
Tobias Bechen <tobias.bechen@gmail.com>
2021-11-09 22:15:17 +0100

    Interfaces

---
+++ katas/Palindrom/solutions/tobi/Palindrom/PalindromFinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    public interface IPalindromFinder
+    {
+        bool IsPalindrom(string text);
+    }
+
+    public class PalindromFinder : IPalindromFinder
+    {
+        public bool IsPalindrom(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
