4f1674a684352371553911b65cf99f7b5eaeb80c
Tobias Bechen <tobias.bechen@gmail.com>
2021-11-09 21:36:40 +0100

    Test Setup

--- katas/FizzBuzz/solutions/tobi/FizzBuzz.Tests/FizzBuzzTests.cs
+++ katas/FizzBuzz/solutions/tobi/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -9,7 +9,35 @@ namespace FizzBuzz.Tests
 {
     public class FizzBuzzTests
     {
-        [Fact]
-        public void GetFizzBuzz_
+        public static List<object[]> ConvertNumberData = new List<object[]>();
+        public static List<object[]> FizzBuzzSimpleData = new List<object[]>();
+        public static List<object[]> FizzBuzzVariationData = new List<object[]>();
+
+        [Theory]
+        [MemberData(nameof(ConvertNumberData))]
+        public void ConvertNumber_ReturnsCorrectResult(int number, bool extended, string expectedResult)
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.ConvertNumber(number, extended);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(FizzBuzzSimpleData))]
+        public void FizzBuzzSimple_ReturnsCorrectResult(int start, int end, string expectedResult)
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.FizzBuzzSimple(start, end);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(FizzBuzzVariationData))]
+        public void FizzBuzzVariation_ReturnsCorrectResult(int start, int end, string expectedResult)
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.FizzBuzzVariation(start, end);
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
