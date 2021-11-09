d5cfc8c99404678f4faa5a4209f17199db43577f
Tobias Bechen <tobias.bechen@gmail.com>
2021-11-09 21:27:57 +0100

    Interfaces

---
+++ katas/FizzBuzz/solutions/tobi/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void GetFizzBuzz_
+    }
+}
---
+++ katas/FizzBuzz/solutions/tobi/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public interface IFizzBuzz
+    {
+        string ConvertNumber(int number, bool extended);
+        string FizzBuzzSimple(int start = 1, int end = 100);
+        string FizzBuzzVariation(int start = 1, int end = 100);
+    }
+
+    public class FizzBuzz : IFizzBuzz
+    {
+        public string ConvertNumber(int number, bool extended)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FizzBuzzSimple(int start = 1, int end = 100)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FizzBuzzVariation(int start = 1, int end = 100)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
