054099358f8f4ca71d7e41a3ab29048761b22de5
Tobias Bechen <tobias.bechen@gmail.com>
2021-11-09 22:38:28 +0100

    Palindrom Check

--- katas/Palindrom/solutions/tobi/Palindrom.Tests/PalindromFinderTests.cs
+++ katas/Palindrom/solutions/tobi/Palindrom.Tests/PalindromFinderTests.cs
@@ -9,7 +9,22 @@ namespace Palindrom.Tests
 {
     public class PalindromFinderTests
     {
-        public static List<object[]> IsPalindromTestData = new List<object[]>();
+        public static List<object[]> IsPalindromTestData = new List<object[]>()
+        {
+            new object[] { "Abba", true },
+            new object[] { "Lagerregal", true },
+            new object[] { "Reliefpfeiler", true },
+            new object[] { "Rentner", true },
+            new object[] { "Dienstmannamtsneid", true },
+            new object[] { "Tarne nie deinen Rat!", true },
+            new object[] { "Eine güldne, gute Tugend: Lüge nie!", true },
+            new object[] { "Ein agiler Hit reizt sie. Geist?! Biertrunk nur treibt sie. Geist ziert ihre Liga nie!", true },
+            new object[] { "Palindrom", false },
+            new object[] { "Abbx", false },
+            new object[] { "a", true },
+            new object[] { "", true },
+            new object[] { null, true },
+        };
 
         [Theory]
         [MemberData(nameof(IsPalindromTestData))]
@@ -17,6 +32,15 @@ namespace Palindrom.Tests
         {
             var checker = new PalindromChecker();
             var result = checker.IsPalindrom(text);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(IsPalindromTestData))]
+        public void IsPalindromeRecursive_ReturnsCorrectResult(string text, bool expected)
+        {
+            var checker = new PalindromChecker();
+            var result = checker.IsPalindromeRecursive(text);
             Assert.Equal(expected, result);
         }
     }
