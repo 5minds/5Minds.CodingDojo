5b377a97d68fa6eac2442cd8c67d2f18c19668c9
Waltraut <95751802+Waltraut@users.noreply.github.com>
2021-12-08 17:25:39 +0100

    Add files via upload

---
+++ katas/Palindrom/solutions/Waltraut/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean IsPalindrom(String Source)
+            {
+                var i = 0;
+                var j = Source.Length - 1;
+                var threshold = System.Math.Ceiling((decimal)j / 2);
+
+                while (i < threshold && Source[i].Equals(Source[j - i])) i++;
+
+                return i == threshold;
+            }
+
+            String ApplySpecialRules(String Source)
+            {
+                return Regex.Replace(Source?.ToUpper() ?? String.Empty, "[^A-Z]", String.Empty);
+            }
+
+            var tests = new List<String> {
+                "Abba", --- ignore case
+                "Lagerregal",
+                "Reliefpfeiler",
+                "Rentner",
+                "Dienstmannamtsneid",
+                "Tarne nie deinen Rat!",
+                "Eine güldne, gute Tugend: Lüge nie!",
+                "Ein agiler Hit reizt sie. Geist?! Biertrunk nur treibt sie. Geist ziert ihre Liga nie!",
+                "a",
+                "",
+                null,
+                "abca"};
+
+            tests.ForEach(item => Console.WriteLine($"Is '{item ?? "null value"}' an palindrom? {IsPalindrom(ApplySpecialRules(item))}"));
+            
+            Console.ReadLine();
+        
+        }
+    }
+}
