484e7b65ce9fb83c107983afc01336e09f90bdfc
Markus Wieczorek <markus.wieczorek@healex.systems>
2021-12-10 19:03:43 +0100

    test: Add addtl testcases
    - These are the other ones from the `README.md`

--- katas/NextBiggerNumber/solutions/mw/NextGreaterNumber/NextGreaterNumber.Test/NextGreaterNumberTest.cs
+++ katas/NextBiggerNumber/solutions/mw/NextGreaterNumber/NextGreaterNumber.Test/NextGreaterNumberTest.cs
@@ -7,7 +7,7 @@ namespace NextGreaterNumber.Test
     [TestClass]
     public class NextGreaterNumberTest
     {
-                private NextGreaterNumber _nextGreaterNumber;
+        private NextGreaterNumber _nextGreaterNumber;
         
         [TestInitialize]
         public void Setup()
@@ -41,6 +41,48 @@ namespace NextGreaterNumber.Test
         {
             var testResult = _nextGreaterNumber.FindNextGreaterNumber(459853);
             testResult.Should().Be(483559);
+        }
+        
+        [TestMethod]
+        public void NextGreaterNumber_GreaterPossible59884848459853Success()
+        {
+            var testResult = _nextGreaterNumber.FindNextGreaterNumber(59884848459853);
+            testResult.Should().Be(59884848483559);
+        }
+        
+        [TestMethod]
+        public void NextGreaterNumber_NoGreaterPossibleFailure()
+        {
+            var testResult = _nextGreaterNumber.FindNextGreaterNumber(9);
+            testResult.Should().Be(-1);
+        }
+        
+        [TestMethod]
+        public void NextGreaterNumber_NoGreaterPossible111Failure()
+        {
+            var testResult = _nextGreaterNumber.FindNextGreaterNumber(111);
+            testResult.Should().Be(-1);
+        }
+        
+        [TestMethod]
+        public void NextGreaterNumber_NoGreaterPossible531Failure()
+        {
+            var testResult = _nextGreaterNumber.FindNextGreaterNumber(531);
+            testResult.Should().Be(-1);
+        }
+
+        [TestMethod]
+        public void NextGreaterNumber_NoGreaterPossibleNegativeFailure()
+        {
+            var testResult = _nextGreaterNumber.FindNextGreaterNumber(-1);
+            testResult.Should().Be(-1);
+        }
+
+        [TestMethod]
+        public void NextGreaterNumber_MaxLongNoGreaterFailure()
+        {
+            var testResult = _nextGreaterNumber.FindNextGreaterNumber(long.MaxValue);
+            testResult.Should().Be(-1);
         }
     }
 }
