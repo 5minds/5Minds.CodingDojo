e54406a50066df3626bdd6d61d72100441541524
Markus Wieczorek <markus.wieczorek@healex.systems>
2021-12-10 19:31:13 +0100

    feat: Add implementation of `BinarySearch.go`

--- katas/Rekursion/solutions/mw/src/BinarySearch.go
+++ katas/Rekursion/solutions/mw/src/BinarySearch.go
@@ -1,1 +1,21 @@
 package binarySearch
+
+func BinarySearch(inputArray []int, target int) int {
+	if len(inputArray) == 0 { // we've gone through it all
+		return -1
+	}
+
+	// helper variables
+
+	low := 0
+	high := len(inputArray) - 1
+	mid := (low + high) / 2
+
+	if inputArray[mid] > target { // take left partial array
+		return BinarySearch(inputArray[:mid], target)
+	} else if inputArray[mid] < target { // take right partial array
+		return BinarySearch(inputArray[mid+1:], target)
+	} else {
+		return mid // target is at mid of array
+	}
+}
