58667e1c4594c760b301c538c073cd255ac084e2
Dzmitry Syrtsou <dmitrysyrtsov@gmail.com>
2022-02-15 21:48:49 +0100

    prepare HTML+CSS mock-up

--- Recruiting.SyrtsouD.Holidays/Recruiting.SyrtsouD.Holidays.API/Resolvers/CalendarificConfigurationsResolver.cs
+++ Recruiting.SyrtsouD.Holidays/Recruiting.SyrtsouD.Holidays.API/Resolvers/CalendarificConfigurationsResolver.cs
@@ -6,11 +6,13 @@ namespace Recruiting.SyrtsouD.Holidays.API.Resolvers
 	{
 		public string ResolveApiKey()
 		{
+			// to be moved to configuration
 			return "0d72917fdf5362a4e525a25731642543794ac4d7";
 		}
 
 		public Uri ResolveBaseUri()
 		{
+			// to be moved to configuration
 			return new Uri("https://calendarific.com/api/v2/", UriKind.Absolute);
 		}
 	}
