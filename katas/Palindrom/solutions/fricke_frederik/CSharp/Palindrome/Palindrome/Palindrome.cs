5224f6316621589bfcac29133786d7fd33121461
Frederik Fricke <seumalp@gmail.com>
2022-03-19 14:35:55 +0100

    feat(csharp-extended): extended palindrome for sentences

--- katas/Palindrom/solutions/fricke_frederik/CSharp/Palindrome/Palindrome/Palindrome.cs
+++ katas/Palindrom/solutions/fricke_frederik/CSharp/Palindrome/Palindrome/Palindrome.cs
@@ -5,6 +5,17 @@ public class Palindrome
     public bool IsPalindrome(string checkString)
     {
         checkString = checkString.ToLower();
+        checkString = checkString.Replace(" ", "")
+            .Replace("!", "")
+            .Replace(".", "")
+            .Replace("?", "")
+            .Replace(",", "")
+            .Replace(":", "")
+            .Replace("\n", "")
+            .Replace("\t", "")
+            .Replace("\r", "")
+            .Replace("\n", "");
+        
         int length = checkString.Length - 1;
         for (int counter = 0; counter <= length ; counter++)
         {
