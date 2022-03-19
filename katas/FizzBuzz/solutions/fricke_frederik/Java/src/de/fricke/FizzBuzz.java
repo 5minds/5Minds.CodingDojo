99d05f464db0d29b29be5a89c2a03ad1c7da3d10
Frederik Fricke <seumalp@gmail.com>
2022-03-19 13:17:17 +0100

    feat(java-impl): implementation of FizzBuzz function

--- katas/FizzBuzz/solutions/fricke_frederik/Java/src/de/fricke/FizzBuzz.java
+++ katas/FizzBuzz/solutions/fricke_frederik/Java/src/de/fricke/FizzBuzz.java
@@ -6,6 +6,24 @@ public class FizzBuzz {
     {
         String outString = "";
 
+        if (numToCheck % 3 == 0) outString += "Fizz";
+        if (numToCheck % 5 == 0) outString += "Buzz";
+
+        outString = CheckString(numToCheck, outString);
+
+        if (outString.equals("")) outString = Integer.toString(numToCheck);
+
+        return outString;
+    }
+
+    private String CheckString(int numToCheck, String prevFizzBuzzString)
+    {
+        String outString = prevFizzBuzzString;
+        String numString = Integer.toString(numToCheck);
+
+        if (numString.contains("3") && !(outString.contains("Fizz"))) outString = "Fizz" + outString;
+        if (numString.contains("5") && !(outString.contains("Buzz"))) outString += "Buzz";
+
         return outString;
     }
 }
