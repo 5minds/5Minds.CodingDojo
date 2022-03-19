610d796fff13327c741fbfde8cfbf631970d6845
Frederik Fricke <seumalp@gmail.com>
2022-03-19 15:07:41 +0100

    feat(java-impl): implemented main function and configured for runnable jar

--- katas/Palindrom/solutions/fricke_frederik/Java/src/de/fricke/Main.java
+++ katas/Palindrom/solutions/fricke_frederik/Java/src/de/fricke/Main.java
@@ -1,8 +1,19 @@
 package de.fricke;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+	    System.out.println("Type in a word or sentence to check for palindrome: ");
+        Scanner scanner = new Scanner(System.in);
+        String palinToCheck = scanner.nextLine();
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.IsPalindrome(palinToCheck)
+                ? "This is a palindrome!"
+                : "This is not a palindrome!");
+
+        scanner.nextLine();
     }
 }
