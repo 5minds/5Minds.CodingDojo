2c71daa45ec21ea807291d22276392a406be41dc
Frederik Fricke <seumalp@gmail.com>
2022-03-19 14:55:12 +0100

    feat(java-impl): implemented the simple palindrome check

--- katas/Palindrom/solutions/fricke_frederik/Java/src/de/fricke/Palindrome.java
+++ katas/Palindrom/solutions/fricke_frederik/Java/src/de/fricke/Palindrome.java
@@ -1,8 +1,16 @@
 package de.fricke;
 
+import java.util.Locale;
+
 public class Palindrome {
     public boolean IsPalindrome(String textToCheck)
     {
-        return false;
+        textToCheck = textToCheck.toLowerCase();
+        int length = textToCheck.length() - 1;
+        for (int counter = 0; counter <= length; counter++)
+        {
+            if (!(textToCheck.charAt(counter) == textToCheck.charAt(length - counter))) return false;
+        }
+        return true;
     }
 }
