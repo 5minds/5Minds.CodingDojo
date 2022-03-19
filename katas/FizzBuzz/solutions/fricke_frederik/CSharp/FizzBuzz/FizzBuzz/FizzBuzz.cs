fc51ebf225ce90512bab0bf8b997d00e3cf2a46e
Frederik Fricke <seumalp@gmail.com>
2022-03-19 13:15:11 +0100

    fix(csharp): fixed CheckString method

--- katas/FizzBuzz/solutions/fricke_frederik/CSharp/FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ katas/FizzBuzz/solutions/fricke_frederik/CSharp/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,46 +6,38 @@ public class FizzBuzz
     /// This is the primary function which checks if the given number is
     /// Fizz, Buzz or FizzBuzz
     /// </summary>
-    /// <param name="intToCheck">Number to check for FizzBuzz</param>
+    /// <param name="numToCheck">Number to check for FizzBuzz</param>
     /// <returns>Returns a string which has Fizz, Buzz or FizzBuzz</returns>
-    public string IsFizzBuzz(int intToCheck)
+    public string IsFizzBuzz(int numToCheck)
     {
         string outString = "";
-        
-        if (intToCheck % 3 == 0)
-        {
-            outString = intToCheck % 5 == 0 ? "FizzBuzz" : "Fizz";
-        } else if (intToCheck % 5 == 0)
-        {
-            outString = "Buzz";
-        }
-        
-        if (outString != "") return outString;
-        CheckString(intToCheck, out outString);
 
-        if (outString == "") outString = intToCheck.ToString();
+
+        if (numToCheck % 3 == 0) outString += "Fizz";
+        if (numToCheck % 5 == 0) outString += "Buzz";
+
+        outString = CheckString(numToCheck, outString);
+
+        if (outString == "") outString = numToCheck.ToString();
         
         return outString;
     }
-    
+
     /// <summary>
     /// A helper function used to search if the given number has
     /// the number 3 or 5 in it.
     /// </summary>
-    /// <param name="intToCheck">The given number which shall be checked</param>
+    /// <param name="numToCheck">The given number which shall be checked</param>
     /// <param name="outString">A string which will returned with Fizz, Buzz or FizzBuzz</param>
-    private void CheckString(int intToCheck, out string outString)
+    /// <param name="prevFizzBuzzString">the previous FizzBuzz-String</param>
+    private string CheckString(int numToCheck, string prevFizzBuzzString)
     {
-        outString = "";
-        
-        string intString = intToCheck.ToString();
-        if (intString.Contains('3'))
-        {
-            outString = intString.Contains('5') ? "FizzBuzz" : "Fizz";
-        }
-        else if (intString.Contains('5'))
-        {
-            outString = "Buzz";
-        }
+        string intString = numToCheck.ToString();
+        string outString = prevFizzBuzzString;
+
+        if (intString.Contains('3') && !(outString.Contains("Fizz"))) outString = "Fizz" + outString;
+        if (intString.Contains('5') && !(outString.Contains("Buzz"))) outString += "Buzz";
+
+        return outString;
     }
 }
