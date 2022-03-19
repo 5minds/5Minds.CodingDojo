31fdde54eaf3a19cd64a9376d4b73c1a08c2c94d
Frederik Fricke <seumalp@gmail.com>
2022-03-19 14:41:31 +0100

    feat(java-init): initial commit for java impementation

---
+++ katas/Palindrom/solutions/fricke_frederik/Java/src/de/fricke/Palindrome.java
@@ -0,0 +1,8 @@
+package de.fricke;
+
+public class Palindrome {
+    public boolean IsPalindrome(String textToCheck)
+    {
+        return false;
+    }
+}
---
+++ katas/Palindrom/solutions/fricke_frederik/Java/src/de/fricke/test/PalindromeTest.java
@@ -0,0 +1,4 @@
+package de.fricke.test;
+
+public class PalindromeTest {
+}
