c030be8eb1588b636b300d590cd385efda1be2f2
Vakilpour <arsess@vakilpour.com>
2022-04-01 13:08:02 +0200

    Adding a few comments!

--- katas/FizzBuzz/solutions/Arsess/js/script.js
+++ katas/FizzBuzz/solutions/Arsess/js/script.js
@@ -6,7 +6,11 @@ let secondNumber = new SpecialNumber( 5 , "Buzz" );
 let outputString = "";
 
 for ( let i = 1; i < 101; i++) {
+
+    // Convert the number to string.
     outputString = i.toString();
+
+    // Check the number ...
     if ( firstNumber.checkNumber( i ) && ! secondNumber.checkNumber( i ) ) {
         outputString = firstNumber.special_word;
     }
@@ -16,5 +20,7 @@ for ( let i = 1; i < 101; i++) {
     else if ( firstNumber.checkNumber( i ) && secondNumber.checkNumber( i ) ) {
         outputString = firstNumber.special_word + secondNumber.special_word;
     }
+
+    // Send output to the client.
     content.innerHTML +=  outputString + "<br />";
 }
