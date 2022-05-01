7ec4c17a9b981c3a41c445de6f83c7abb7ae1c92
daniel <blacksemid@gmail.com>
2022-05-01 12:54:08 +0200

    CreateProject

---
+++ katas/2018-02-21_Doubletten/solutions/zaksek_c#/FindDuplicates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
